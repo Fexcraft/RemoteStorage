eca6444665fb6f1c8dd990b5d5092fb297a77820
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-11 18:20:53 +0200

    Update AuthContainer.java

--- src/main/java/net/fexcraft/mod/remotestorage/gui/AuthContainer.java
+++ src/main/java/net/fexcraft/mod/remotestorage/gui/AuthContainer.java
@@ -1,6 +1,8 @@
 package net.fexcraft.mod.remotestorage.gui;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
+import net.fexcraft.mod.remotestorage.GroupManager;
+import net.fexcraft.mod.remotestorage.GroupManager.Group;
 import net.fexcraft.mod.remotestorage.RemoteStorage;
 import net.fexcraft.mod.remotestorage.block.TransferTE;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,17 +13,20 @@ import net.minecraftforge.fml.relauncher.Side;
 public class AuthContainer extends GenericContainer {
 	
 	private TransferTE tile;
+	private Group group;
 
 	public AuthContainer(EntityPlayer player, int x, int y, int z){
 		super(player);
 		tile = (TransferTE)player.world.getTileEntity(new BlockPos(x, y, z));
+		if(!player.world.isRemote) group = GroupManager.of(player);
 	}
 
 	@Override
 	protected void packet(Side side, NBTTagCompound packet, EntityPlayer player){
 		if(side.isServer()){
-			tile.auth = packet.getString("auth");
+			tile.auth = packet.getString("org");
 			tile.token = packet.getString("tok");
+			tile.company = group.id;
             player.openGui(RemoteStorage.INSTANCE, 1, player.world, tile.getPos().getX(), tile.getPos().getY(), tile.getPos().getZ());
 		}
 	}
