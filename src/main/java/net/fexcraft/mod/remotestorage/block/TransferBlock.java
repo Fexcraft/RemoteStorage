5e1490e973dbbdff560b9debd2cb0842ce9d6efa
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-11 17:47:15 +0200

    Update TransferBlock.java

--- src/main/java/net/fexcraft/mod/remotestorage/block/TransferBlock.java
+++ src/main/java/net/fexcraft/mod/remotestorage/block/TransferBlock.java
@@ -1,6 +1,9 @@
 package net.fexcraft.mod.remotestorage.block;
 
 import net.fexcraft.lib.mc.api.registry.fBlock;
+import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.remotestorage.GroupManager;
+import net.fexcraft.mod.remotestorage.GroupManager.Group;
 import net.fexcraft.mod.remotestorage.RemoteStorage;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
@@ -28,12 +31,27 @@ public class TransferBlock extends BlockContainer {
 	
 	@Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ){
-        if(world.isRemote) return false;
+        if(world.isRemote || hand == EnumHand.OFF_HAND) return false;
         if(player.isSneaking()) return true;
-        //todo perm check
-        //todo auth state check
-        player.openGui(RemoteStorage.INSTANCE, 0, world, pos.getX(), pos.getY(), pos.getZ());
-        return true;
+        TransferTE tile = (TransferTE)world.getTileEntity(pos);
+        if(tile == null) return false;
+        Group group = GroupManager.of(player);
+        if(group == null){
+        	Print.chat(player, "Please join a company first.");
+        	return true;
+        }
+        if(tile.auth == null){
+            player.openGui(RemoteStorage.INSTANCE, 0, world, pos.getX(), pos.getY(), pos.getZ());
+            return true;
+        }
+        if(tile.company.equals(group.id)){
+            player.openGui(RemoteStorage.INSTANCE, 1, world, pos.getX(), pos.getY(), pos.getZ());
+            return true;
+        }
+        else{
+        	Print.chat(player, "This transfer block is not registered under your company.");
+            return true;
+        }
     }
 	
 	@Override
