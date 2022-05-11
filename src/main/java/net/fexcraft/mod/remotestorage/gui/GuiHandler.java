7ecb4e30c877e7dc2a8dd01b3986b35613959d80
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-11 18:20:56 +0200

    Update GuiHandler.java

--- src/main/java/net/fexcraft/mod/remotestorage/gui/GuiHandler.java
+++ src/main/java/net/fexcraft/mod/remotestorage/gui/GuiHandler.java
@@ -9,12 +9,14 @@ public class GuiHandler implements IGuiHandler {
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
 		if(ID == 0) return new AuthContainer(player, x, y, z);
+		if(ID == 1) return new TransferContainer(player, x, y, z);
 		return null;
 	}
 	
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
 		if(ID == 0) return new AuthGui(player, x, y, z);
+		if(ID == 1) return new TransferGui(player, x, y, z);
 		return null;
 	}
 	
